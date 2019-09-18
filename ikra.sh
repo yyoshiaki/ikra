596289681dbb2b55e4ca67cf01a1e168605414cd
kaitoou <k.ou.medical@gmail.com>
2019-09-18 14:05:54 +0900

    実行後、不要な中間ファイルを取り除くoptionを追加しました。

--- ikra.sh
+++ ikra.sh
@@ -67,6 +67,7 @@ IF_PC=false
 SUFFIX_PE_1=_1.fastq.gz
 SUFFIX_PE_2=_2.fastq.gz
 OUTPUT_FILE=output.tsv
+REMOVE=false
 
 # オプションをパース
 PARAM=()
@@ -127,6 +128,9 @@ for opt in "$@"; do
         '-v' | '--version' )
             usage
             ;;
+        '-r' | '--remove' )
+            REMOVE=true ; shift
+           ;;
         '--' | '-' )
             shift
             PARAM+=( "$@" )
@@ -564,6 +568,13 @@ if [[  -f "tximport_R.R" ]]; then
   rm tximport_R.R
 fi
 
+if [ $REMOVE = true ]; then
+  rm -f *fastq.gz
+  rm -f *fq.gz
+  rm -f gencode*.gz
+  rm -f *fastqc.zip
+  rm -rf salmon_output_*
+fi
 # if [[ "$RUNINDOCKER" -eq "1" ]]; then
 #
 #   chmod 755 .
