91667074f015758691785d30f2c0a6a765094c60
yyoshiaki <y.yasumizu@gmail.com>
2019-10-06 11:26:12 +0900

    test.full.sh added for validate fasterq-dump

--- test/test.full.sh
+++ test/test.full.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# test modeをつけないバージョン。fasterq-dumpをテストするならこちら。
+
 cd Illumina_SE 
 
 # csv以外を削除
 ls | grep -v -E '*.csv' | xargs rm -r
 
-bash ../../ikra.sh Illumina_SE_SRR.csv mouse --test -t 6
+bash ../../ikra.sh Illumina_SE_SRR.csv mouse -t 6
 
 rm -r salmon_*
 rm -r multiqc*
@@ -18,7 +20,7 @@ cd ../Illumina_PE
 # csv以外を削除
 ls | grep -v -E '*.csv' | xargs rm -r
 
-bash ../../ikra.sh Illumina_PE_SRR.csv mouse --test -t 6
+bash ../../ikra.sh Illumina_PE_SRR.csv mouse -t 6
 
 rm -r salmon_*
 rm -r multiqc*
